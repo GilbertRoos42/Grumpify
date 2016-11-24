cad818b0a7965ab0379bf831322d682ba7638e89
Unknown <GilbertRoos42@gmail.com>
2016-11-23 19:00:51 -0500

    Added grumpify.js and manifest.json

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Grumpify
+This is a super well done Chrome extension that takes all those annoying non Grumpy Cat images on a webpage, and turns them into Grumpy Cat. Yay!
---
+++ grumpify.js
@@ -0,0 +1,5 @@
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  //images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+    images[i].src = 'http://s1.thingpic.com/images/nb/PUSCYKU1mfpJYiZG6B9WDF1R.jpeg';
+}
