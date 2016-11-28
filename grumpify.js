a1b668dcec8e52faeaf38fb5321ee2b341aaf239
gibby roos <GilbertRoos42@gmail.com>
2016-11-27 20:17:31 -0500

    Removed uncessary console.log statements

--- grumpify.js
+++ grumpify.js
@@ -1,14 +1,11 @@
-console.log('Before document.ready');
 $(document).ready(function()
 {
         var images = document.getElementsByTagName('img');
         for (var i = 0, l = images.length; i < l; i++) {
         images[i].src = 'http://s1.thingpic.com/images/nb/PUSCYKU1mfpJYiZG6B9WDF1R.jpeg';
         }
-        console.log('Inside document.ready');
         $(document).on("scroll change ",function()
         {
-            console.log('inside document.live');
             var images = document.getElementsByTagName('img');
             for (var i = 0, l = images.length; i < l; i++) {
             images[i].src = 'http://s1.thingpic.com/images/nb/PUSCYKU1mfpJYiZG6B9WDF1R.jpeg';
